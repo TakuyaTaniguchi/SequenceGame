6c171715f97926aaf101686e9d3c585a7d9d90d5
ttaniguchi <ttaniguchi@oikaze.jp>
2019-06-26 10:10:09 +0900

    Update 変数の定義ができてなかったので修正

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const startBox = document.querySelector('.startBox');
 const moveBox = document.querySelector('.sequence_bar_movebox');
 const hitbox = document.querySelector('.sequence_bar_hitbox');
 
+
+
 //hitboxのサイズを取得
 const clearArea = hitbox.offsetLeft;
 const clearAreaEnd = hitbox.offsetLeft + 30;
@@ -20,8 +22,8 @@ stopBox.addEventListener('click',(event)=>{
     moveBox.classList.add('-is-stop');
     startBox.style.display = "inline-block";
     event.target.style.display = "none";
-    moveBoxLeft = moveBox.offsetLeft;
-    moveBoxRight = moveBox.offsetLeft + 10;
+    const moveBoxLeft = moveBox.offsetLeft;
+    const moveBoxRight = moveBox.offsetLeft + 10;
     if(clearArea <= moveBoxLeft && clearAreaEnd >= moveBoxLeft ){
         alert('Clear');
     }else if(clearArea <= moveBoxRight  && clearAreaEnd >= moveBoxRight  ){
