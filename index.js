3a7640887957e8310ce0dfab6190de54ffcb55ea
ttaniguchi <ttaniguchi@oikaze.jp>
2019-06-26 10:42:44 +0900

    Update 関数に切り出し。

--- index.js
+++ index.js
@@ -11,28 +11,44 @@ const startBox = document.querySelector('.startBox');
 const moveBox = document.querySelector('.sequence_bar_movebox');
 const hitbox = document.querySelector('.sequence_bar_hitbox');
 
-
-
 //hitboxのサイズを取得
 const clearArea = hitbox.offsetLeft;
-const clearAreaEnd = hitbox.offsetLeft + 30;
+const clearAreaEnd = hitbox.offsetLeft + hitbox.clientWidth;
 
+//moveboxをスタート,ストップする。
+const changeMoveAction = () => {
+    moveBox.classList.toggle('-is-stop');
+}
 
-stopBox.addEventListener('click',(event)=>{
-    moveBox.classList.add('-is-stop');
+const hideStopBox = () => {
     startBox.style.display = "inline-block";
     event.target.style.display = "none";
-    const moveBoxLeft = moveBox.offsetLeft;
-    const moveBoxRight = moveBox.offsetLeft + 10;
+}
+
+const hideStartBox = () =>{
+    stopBox.style.display = "inline-block";
+    event.target.style.display = "none";
+}
+
+//moveBoxがclearArea,clearAreaEndのwidth内にある場合はtrueを返す。
+const clearJudge　= (clearArea,clearAreaEnd,moveBoxLeft,moveBoxRight) =>{
     if(clearArea <= moveBoxLeft && clearAreaEnd >= moveBoxLeft ){
-        alert('Clear');
+        return alert('Clear');
     }else if(clearArea <= moveBoxRight  && clearAreaEnd >= moveBoxRight  ){
-        alert('Clear');
+        return alert('Clear');
     }
+}
+
+stopBox.addEventListener('click',(event)=>{
+    changeMoveAction();
+    hideStopBox();
+    //moveboxのサイズを取得
+    const moveBoxLeft = moveBox.offsetLeft;
+    const moveBoxRight = moveBox.offsetLeft + moveBox.clientWidth;
+    clearJudge(clearArea,clearAreaEnd,moveBoxLeft,moveBoxRight);
  });
 
 startBox.addEventListener('click',(event)=>{
-    moveBox.classList.remove('-is-stop');
-    stopBox.style.display = "inline-block";
-    event.target.style.display = "none";
+    changeMoveAction();
+    hideStartBox();
  });
